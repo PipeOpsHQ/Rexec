56387f53408d43051448a9a09b3ff836c6ce4a19
Alex Idowu <alexidowu25@gmail.com>
2025-12-04 14:46:06 +0100

    style: Improve styles for upload and download buttons in TerminalPanel

--- web-ui/src/lib/components/terminal/TerminalPanel.svelte
+++ web-ui/src/lib/components/terminal/TerminalPanel.svelte
@@ -473,31 +473,31 @@
                 style="display: none;"
             />
             <button
-                class="toolbar-btn icon-btn"
+                class="toolbar-btn icon-btn upload-btn"
                 on:click={handleUploadClick}
                 disabled={isUploading || !isConnected}
-                title="Upload File"
+                title="Upload File to Terminal"
             >
                 {#if isUploading}
                     <div class="mini-spinner"></div>
                 {:else}
                     <svg class="toolbar-icon" viewBox="0 0 16 16" fill="currentColor">
-                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                        <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
+                        <path d="M8 0a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 12.293V.5A.5.5 0 0 1 8 0z" transform="rotate(180 8 8)"/>
+                        <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/>
                     </svg>
                 {/if}
             </button>
             
             <!-- File Download -->
             <button
-                class="toolbar-btn icon-btn"
+                class="toolbar-btn icon-btn download-btn"
                 on:click={handleDownloadClick}
                 disabled={!isConnected}
-                title="Download File"
+                title="Download File from Terminal"
             >
                 <svg class="toolbar-icon" viewBox="0 0 16 16" fill="currentColor">
-                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                    <path d="M8 15a.5.5 0 0 1-.5-.5V1.707L4.354 4.854a.5.5 0 1 1-.708-.708l4-4a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1-.708.708L8.5 1.707V14.5a.5.5 0 0 1-.5.5z" transform="rotate(180 8 8)"/>
+                    <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/>
                 </svg>
             </button>
             
@@ -704,6 +704,8 @@
         /* Hide scrollbar but allow scrolling */
         scrollbar-width: none;
         -ms-overflow-style: none;
+        position: relative;
+        z-index: 100;
     }
 
     .terminal-toolbar::-webkit-scrollbar {
@@ -955,6 +957,20 @@
         color: var(--green);
         border-color: var(--green);
         background: rgba(0, 255, 136, 0.1);
+    }
+
+    /* Upload Button */
+    .toolbar-btn.upload-btn:hover {
+        color: #4fc3f7;
+        border-color: #4fc3f7;
+        background: rgba(79, 195, 247, 0.1);
+    }
+
+    /* Download Button */
+    .toolbar-btn.download-btn:hover {
+        color: #81c784;
+        border-color: #81c784;
+        background: rgba(129, 199, 132, 0.1);
     }
 
     /* Split Button */
